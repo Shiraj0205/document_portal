74d8b33aa49e6d601104f19b2329d4b598ed0e37
shiraj0205 <shiraj.shaikh@gmail.com>
2025-07-20 01:23:51 -0500

    Add package configuration and update folder structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='document_portal',
+    version='0.1.0',
+    description='Document processing and Management Portal',
+    author='Shiraj S',
+    packages=find_packages()
+)
