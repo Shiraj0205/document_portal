eb0f3713f86a46c313dc3166f1b02d5857bd3d12
shiraj0205 <shiraj.shaikh@gmail.com>
2025-07-30 23:41:10 -0500

    Data ingestion logic added

---
+++ src/document_analyzer/data_analysis.py
@@ -0,0 +1,20 @@
+import os
+from utils.model_loader import ModelLoader
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+from model.models import *
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.output_parsers import OutputFixingParser
+
+
+class DocumentAnalyzer:
+
+    """Document Analyzer Class
+    """
+    def __init__(self):
+        pass
+
+    def analyze_document(self):
+        """Analyze the document
+        """
+        pass
--- utils/config_loader.py
+++ utils/config_loader.py
@@ -3,7 +3,9 @@ import yaml
 def load_config(config_path: str = "config\config.yaml") -> dict:
     with open(config_path, 'r') as file:
         config = yaml.safe_load(file)
-        print(config)
+        #print(config)
     return config
 
-# load_config()
+
+if __name__ == "__main__":
+    load_config()
