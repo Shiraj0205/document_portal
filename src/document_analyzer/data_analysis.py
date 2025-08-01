b9b8598bdd6bc94ab37400987251cbb0d533cdb4
shiraj0205 <shiraj.shaikh@gmail.com>
2025-07-31 21:53:30 -0500

    Data analysis logic added, prompt added

--- src/document_analyzer/data_analysis.py
+++ src/document_analyzer/data_analysis.py
@@ -5,6 +5,7 @@ from exception.custom_exception import DocumentPortalException
 from model.models import *
 from langchain_core.output_parsers import JsonOutputParser
 from langchain.output_parsers import OutputFixingParser
+from prompt.prompt_library import *
 
 
 class DocumentAnalyzer:
@@ -12,9 +13,38 @@ class DocumentAnalyzer:
     """Document Analyzer Class
     """
     def __init__(self):
-        pass
+        self.log = CustomLogger().get_logger(__name__)
+        try:
+            self.loader = ModelLoader()
+            self.llm = self.loader.load_llm()
 
-    def analyze_document(self):
+            # Prepare parsers
+            self.parser = JsonOutputParser(pydantic_object=Metadata)
+            self.fixing_parser = OutputFixingParser.from_llm(parser=self.parser, llm=self.llm)
+
+            self.prompt = prompt
+            self.log.info("Document analyzer initialized successfully.")
+            
+        except Exception as e:
+            self.log.error(f"Error in initialzing document analyzer. {e}")
+            raise DocumentPortalException(error_message="Error in initialzing document analyzer.", error_details=e) from e
+
+    def analyze_document(self, document_text: str) -> dict:
         """Analyze the document
         """
-        pass
+        try:
+            chain = self.prompt | self.llm | self.fixing_parser
+            self.log.info("Meta data analysis chain initialized")
+
+            response = chain.invoke({
+                "format_instructions": self.parser.get_format_instructions(),
+                "document_text": document_text
+            })
+
+            self.log.info("Meta data execution successful", keys=list(response.keys()))
+            return response
+
+        except Exception as e:
+            self.log.error("Meta data analysis failed", error=str(e))
+            raise DocumentPortalException(error_message="Meta data analysis failed", error_details=e) from e
+        
