97cfd11b22cf5181a485a2cdedcbfb2e0e5a427e
shiraj0205 <shiraj.shaikh@gmail.com>
2025-08-03 22:36:54 -0500

    Implement DocumentAnalyzer and DocumentCompareLLM classes: add file handling, PDF reading, and document comparison logic

--- src/document_compare/data_ingestion.py
+++ src/document_compare/data_ingestion.py
@@ -9,24 +9,71 @@ class DocumentAnalyzer:
     """
         DocumentAnalyzer Class
     """
-
-    def __init__(self):
-        pass
+    def __init__(self, base_dir):
+        self.log = CustomLogger().get_logger(__name__)
+        self.base_dir = Path(base_dir)
+        self.base_dir.mkdir(parents=True, exist_ok=True)
 
     def delete_existing_file(self):
         """
         Delete existing file
         """
-        pass
+        try:
+            if self.base_dir.exists() and self.base_dir.is_dir():
+                for file in self.base_dir.iterdir():
+                    if file.is_file():
+                        file.unlink()
+                        self.log.info("File deleted", directory=str(self.base_dir))
+        except Exception as e:
+            self.log.error("Error deleting document. {e}")
+            raise DocumentPortalException(error_message="Error deleting document", error_details=e) from e
 
-    def save_uploaded_file(self):
+    def save_uploaded_file(self, reference_file, actual_file):
         """
         Save uploaded 
         """
-        pass
+        try:
+            self.delete_existing_file()
+            self.log.info("Existing file deleted successfully")
+            
+            ref_path = self.base_dir/reference_file
+            act_path = self.base_dir/actual_file
 
-    def read_pdf(self):
+            if not reference_file.name.endswith(".pdf") or not actual_file.name.endswith(".pdf"):
+                raise ValueError("Only PDF file are allowed")
+            
+            with open(act_path, "wb") as f:
+                f.write(actual_file.getbuffer())
+            
+            with open(ref_path, "wb") as f:
+                f.write(reference_file.getbuffer())
+
+            self.log.info("Files saved", reference=str(ref_path), actual=str(act_path))
+            return ref_path, act_path
+
+        except Exception as e:
+            self.log.error("Error saving document. {e}")
+            raise DocumentPortalException(error_message="Error saving document", error_details=e) from e
+
+    def read_pdf(self, pdf_path: Path)-> str:
         """
         Read PDF Document
         """
-        pass
+        try:
+            with fitz.open(self, pdf_path) as doc:
+                if doc.is_encrypted:
+                    raise ValueError(f"PDF is encrypted {pdf_path.name}")
+                
+                all_text = []
+                for page_num in range(doc.page_count):
+                    page = doc.load_page(page_num)
+                    text = page.get_text()
+
+                    if text.strip():
+                        all_text.append(f"\n --- Page {page_num + 1} ---\n {text}")
+
+                self.log.info("PDF read successfully", file=str(pdf_path), pages=len(all_text))
+                return "\n".join(all_text)
+        except Exception as e:
+            self.log.error("Error reading document. {e}")
+            raise DocumentPortalException(error_message="Error reading document", error_details=e) from e
--- src/document_compare/document_compare.py
+++ src/document_compare/document_compare.py
@@ -8,6 +8,7 @@ from prompt.prompt_library import PROMPT_REGISTRY
 from utils.model_loader import ModelLoader
 from langchain_core.output_parsers import JsonOutputParser
 from langchain.output_parsers import OutputFixingParser
+import pandas as pd
 
 class DocumentCompareLLM:
     """
@@ -15,16 +16,42 @@ class DocumentCompareLLM:
     """
 
     def __init__(self):
-        pass
+        load_dotenv()
+        self.log = CustomLogger().get_logger(name=__name__)
+        self.loader = ModelLoader()
+        self.llm = self.loader.load_llm()
+        self.parser = JsonOutputParser(pydantic_object=SummaryResponse)
+        self.fixing_parser = OutputFixingParser.from_llm(parser=self.parser, llm=self.llm)
+        self.prompt = PROMPT_REGISTRY.get("document_comparison")
+        self.chain = self.prompt | self.llm | self.parser | self.fixing_parser
+        self.log.info("DocumentCompareLLM class initialized")
 
-    def compare_document(self):
+    def compare_document(self, combined_docs: str):
         """
             Compare two documents
         """
-        pass
+        try:
+            inputs = {
+                "combined_docs": combined_docs,
+                "format_instruction": self.parser.get_format_instructions()
+            }
+            self.log.info("Starting document comparison", inputs=inputs)
+            response = self.chain.invoke(inputs)
+            self.log.info("Document comparison completed", response=response)
+            return self._format_response(response)
+        except Exception as e:
+            self.log.error(f"Error comparing the document {e}")
+            raise DocumentPortalException(error_message="Error comparing the document", error_details=e) from e
 
-    def _format_response(self):
+
+    def _format_response(self, response_parsed: list[dict]) -> pd.DataFrame: #type: ignore
         """
             Format the response from LLM into structured format
         """
-        pass
+        try:
+            df = pd.DataFrame(response_parsed)
+            self.log.info("Response formatted into DataFrame", dataframe=df)
+            return df
+        except Exception as e:
+            self.log.error(f"Error formatting the response {str(e)}")
+            raise DocumentPortalException(error_message="Error formatting the response", error_details=sys) from e
